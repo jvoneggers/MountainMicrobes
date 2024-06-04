28394dfc56f0b8ccb7cac86dc070d00cc31c48f7
Jordan Von Eggers <jordanvoneggers@gmail.com>
2024-06-04 14:12:45 -0700

    Updating starting files

---
+++ 0_SequenceGathering/0_SequenceGathering.sh
@@ -0,0 +1,20 @@
+# Jordan Von Eggers
+# 4 June 2024
+# Here I am 
+
+# Log in 
+ssh jvonegge@beartooth.arcc.uwyo.edu
+
+
+# Copy over Ioana's samples
+cd /project/microbiome/data_queue/seq/ReRun4/rawdata/sample_fastq/16S/SNOTEL
+cp * /gscratch/jvonegge/MountainMicrobes/0_raw_fastq_files
+
+# count number of samples 
+cd /gscratch/jvonegge/MountainMicrobes/0_raw_fastq_files
+
+
+# Copy over Dulcinea's samples
+
+cd /project/microbiome/data_queue/sequence/ReRun3
+
