ab4d002dd4ba6ef6793dc46ea128f6858915cfa4
Ioana C Stefanescu <ioana@Ioanas-Air.lan>
2024-07-12 09:47:13 -0400

    Initial analyses: metadata and 16S Ioana Stefanescu June 12, 2024

--- 3_CreatePhyloseq/3_CreatePhyloseq_ZOTUexact.R
+++ 3_CreatePhyloseq/3_CreatePhyloseq_ZOTUexact.R
@@ -2,14 +2,32 @@
 require(phyloseq)
 require(tidyverse)
 
-setwd("/Users/jordanscheibe/Library/CloudStorage/OneDrive-UniversityofWyoming/LakeSedDNA/Data/MountainMicrobes/4_DataAnalysis")
+
+#Add date and name below if modifying or rerunning this script
+####### 07.09.2024 Modified by Ioana Stefanescu
+
+
+setwd("/Users/ioana/Desktop/MountainMicrobes/4_DataAnalysis")
+
 ### a. read in metadata tables
-metadata<-read.csv(list.files(path="../3_CreatePhyloseq/",pattern = "*_metadata.csv", full.names = TRUE), header=T)
+metadata<-read.csv("/Users/ioana/Desktop/MountainMicrobes/6_MetadataAnalysis/2024-07-08_mountain_microbes_sample_metadata.csv",  header=T)
+metadata<- metadata[!metadata$sample_type == "MC_71", ] # remove the one unidentified soil sample Ioana_G12
 rownames(metadata)<-metadata$sample_id
+
 
 ### b. read in V7 files
 rarefied_esv_table <- read.csv("../2_DataCleaning/ZOTU/2024-06-26_ZOTUexact_ESV_tab_10K_reads_normalized.csv", row.names=1, header=T)
 taxonomy <- read.csv("../2_DataCleaning/ZOTU/2024-06-26_ZOTUexact_tax_tab_notnorm.csv", row.names=1, header=T)
+
+
+#Check which samples are in the ESV file but not in the Metadata
+diffs = setdiff(names(rarefied_esv_table), metadata$sample_id) 
+#print(diffs)
+
+#Check which samples are in the Metadata file but not in the ESV
+diffs = setdiff(metadata$sample_id,names(rarefied_esv_table)) 
+#print(diffs)
+
 
 ### c. Put into Phyloseq
 #put in "Unassigned" in blanks in taxa tab
@@ -22,23 +40,36 @@ tax_tab <- tax_table(tax_tab)
 rm(taxonomy)
 
 # subset metadata data frame by the samples in 'rarefied_esv_table' and convert metadata into a phyloseq object
+
+#check sample number in esv vs metadata
+length(rarefied_esv_table)
+# [1] 616
+length(metadata$sample_id)
+# [1] 653
+# difference of 37 samples 
+
 metadata <- metadata %>% filter(rownames(.) %in% names(rarefied_esv_table))
+length(metadata$sample_id)
+# [1] 609
 samp_dat <- sample_data(metadata)
+
+
+#write.csv(metadata, "/Users/ioana/Desktop/MountainMicrobes/6_MetadataAnalysis/Metadata_for_keptsamples_ZOTUexact.csv")
 
 #convert ESV table into a phyloseq object
 esv_tab_norm <- otu_table(rarefied_esv_table, taxa_are_rows = T)
 rm(rarefied_esv_table)
 
+
 # make phyloseq objects
 ps <- phyloseq(esv_tab_norm, samp_dat, tax_tab)
+
+#transform sample data
+ps_tr <- transform_sample_counts(ps, function(x) x / sum(x))
+variation<-"ZOTUexact"
+
 
 #remove phyloseq sub-objects (not metadata because we want that separately)
 rm(esv_tab_norm)
 rm(tax_tab)
 rm(samp_dat)
-
-#transform sample data
-#ps_tr <- transform_sample_counts(ps, function(x) x / sum(x))
-
-
-variation<-"ZOTUexact"
